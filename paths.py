b67fc64a637c885bd52e3b42ba5df2c5cf34f7bf
Joseph vantassel <jvantassel@utexas.edu>
2020-07-27 16:42:09 -0500

    :sparkles: Get parameter and paths under source control

---
+++ paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '/work/04709/vantaj94/stampede2/seisflows/checkers/model_init'
+
+MODEL_TRUE = '/work/04709/vantaj94/stampede2/seisflows/checkers/model_true'
+
+PRECOND = ''
+
+SPECFEM_DATA = '/work/04709/vantaj94/stampede2/seisflows/checkers/specfem2d/DATA'
+
+SPECFEM_BIN = '/work/04709/vantaj94/stampede2/sesiflows/checkers/specfem2d/bin'
+
